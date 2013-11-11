0b96b69fb8e233782fedfcf281df1f565b0cc21a
Ben Collins-Sussman <sussman@red-bean.com>
2013-11-11 06:22:42 -0600

    gofmt reformatting.

--- cw-decode.go
+++ cw-decode.go
@@ -1,4 +1,4 @@
-/*  
+/*
   Go program to decode an audio stream of Morse code into a stream of ASCII.
 
   See the ALGORITHM file for a description of what's going on, and
@@ -18,19 +18,17 @@ import (
 	"sort"
 )
 
-
 type token int
 
 const (
-	dit = iota
-	dah = iota
+	dit       = iota
+	dah       = iota
 	endLetter = iota
-	endWord = iota
-	pause = iota
-	noOp = iota
-	cwError = iota
+	endWord   = iota
+	pause     = iota
+	noOp      = iota
+	cwError   = iota
 )
-
 
 // ------- Stage 1:  Detect tones in the stream. ------------------
 
@@ -42,13 +40,12 @@ func rms(audiovals []int) int {
 	for i := 0; i < len(audiovals); i++ {
 		v := audiovals[i]
 		sum = sum + v
-		squaresum = squaresum + (v*v)
+		squaresum = squaresum + (v * v)
 	}
 	mean := sum / len(audiovals)
 	meanOfSquares := squaresum / len(audiovals)
 	return int(math.Sqrt(float64(meanOfSquares - (mean * mean))))
 }
-
 
 // Read audiosample chunks from 'chunks' channel, and push simple RMS
 // amplitudes into the 'amplitudes' channel.
@@ -58,7 +55,6 @@ func amplituder(chunks chan []int, amplitudes chan int) {
 	}
 	close(amplitudes)
 }
-
 
 // Read amplitudes from 'amplitudes' channel, and push quantized
 // on/off values to 'quants' channel.
@@ -73,8 +69,12 @@ func quantizer(amplitudes chan int, quants chan bool) {
 		// use that value to quantize each amplitude.
 		group[seen] = amp
 		seen += 1
-		if amp > max { max = amp }
-		if amp < min { min = amp }				
+		if amp > max {
+			max = amp
+		}
+		if amp < min {
+			min = amp
+		}
 		if seen == 100 {
 			middle := (max - min) / 2
 			for i := 0; i < 100; i++ {
@@ -88,7 +88,6 @@ func quantizer(amplitudes chan int, quants chan bool) {
 	close(quants)
 }
 
-
 // Main stage 1 pipeline: reads audiochunks from input channel;
 // returns a boolean channel to which it pushes quantized on/off
 // values.
@@ -100,9 +99,8 @@ func getQuantizePipe(audiochunks chan []int) chan bool {
 	return quants
 }
 
-
 // ------- Stage 2:  Run-length encode the on/off states. ----------
-// 
+//
 // That is, if the input stream is 0001100111100, we want to output
 // the list [3, 2, 2, 4, 2], which can be seen as the "rhythm" of the
 // coded message.
@@ -115,8 +113,8 @@ func getRlePipe(quants chan bool) chan int {
 
 		// TODO(sussman): need to "debounce" this stream
 		for quant := range quants {
-			if quant == currentState { 
-				tally += 1 
+			if quant == currentState {
+				tally += 1
 			} else {
 				lengths <- tally
 				currentState = quant
@@ -128,9 +126,8 @@ func getRlePipe(quants chan bool) chan int {
 	return lengths
 }
 
-
-// ------- Stage 3: Figure out length of morse 'unit' & output logic tokens 
-// 
+// ------- Stage 3: Figure out length of morse 'unit' & output logic tokens
+//
 
 // Take a list of on/off duration events, sort them, return the 25th
 // percentile value as the "1 unit" duration within the time window.
@@ -147,12 +144,11 @@ func calculateUnitDuration(group []int) int {
 	return group[(len(group) / 4)]
 }
 
-
 // Take a normalized duration value, 'clamp' it to the magic numbers
 // 1, 3, 7 (which are the faundational time durations in Morse code),
 // and return a sensible semantic token.
 func clamp(x float32, silence bool) token {
-	if (silence) {
+	if silence {
 		switch {
 		case x > 8:
 			return pause
@@ -174,10 +170,9 @@ func clamp(x float32, silence bool) token {
 		default:
 			return dit
 		}
-	}	
+	}
 	return cwError
 }
-
 
 func getTokenPipe(durations chan int) chan token {
 	tokens := make(chan token)
@@ -212,9 +207,6 @@ func getTokenPipe(durations chan int) chan token {
 	return tokens
 }
 
-
-
-
 // ------ Put all the pipes together. --------------
 
 func chk(err error) {
@@ -223,7 +215,7 @@ func chk(err error) {
 	}
 }
 
-func main () {
+func main() {
 	// Die on Control-C
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
@@ -234,58 +226,65 @@ func main () {
 	// construct main output pipe... whee!
 	output := getTokenPipe(getRlePipe(getQuantizePipe(chunks)))
 
-/*	portaudio.Initialize()
-	defer portaudio.Terminate()
+	/*	portaudio.Initialize()
+		defer portaudio.Terminate()
 
-	in := make([]int32, 64)
-	stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(in), in)
-	chk(err)
-	defer stream.Close()
-	nSamples := 0
+		in := make([]int32, 64)
+		stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(in), in)
+		chk(err)
+		defer stream.Close()
+		nSamples := 0
 
-	go func() {
-		chk(stream.Start())
-		for {
-			chk(stream.Read())
+		go func() {
+			chk(stream.Start())
+			for {
+				chk(stream.Read())
 
-			// chk(binary.Write(f, binary.BigEndian, in))
-			
-			nSamples += len(in)
-			select {
-			case <-sig:
-				return
-			default:
+				// chk(binary.Write(f, binary.BigEndian, in))
+
+				nSamples += len(in)
+				select {
+				case <-sig:
+					return
+				default:
+				}
 			}
+			chk(stream.Stop())
 		}
-		chk(stream.Stop())
-	}
-*/
+	*/
 
 	// Start pushing random data into the pipeline in the background
 	go func() {
-		for i :=0 ; i < 5000; i++ {
+		for i := 0; i < 5000; i++ {
 			chunk := make([]int, 10)
-			for j := 0; j < 10; j++ { chunk[j] = rand.Int() }
+			for j := 0; j < 10; j++ {
+				chunk[j] = rand.Int()
+			}
 			chunks <- chunk
 		}
 		close(chunks)
 	}()
 
-
 	// Print logical tokens from the pipeline's output
 	for val := range output {
 		out := ""
 		switch val {
-		case dit: out = ". "
-		case dah: out = "_ "
-		case endLetter: out = "  "
-		case endWord: out = ": "
-		case pause: out = "pause "
-		case noOp: out = ""
-		default: out = "ERROR "
+		case dit:
+			out = ". "
+		case dah:
+			out = "_ "
+		case endLetter:
+			out = "  "
+		case endWord:
+			out = ": "
+		case pause:
+			out = "pause "
+		case noOp:
+			out = ""
+		default:
+			out = "ERROR "
 		}
 		fmt.Printf("%s", out)
 	}
 	close(output)
 }
-
