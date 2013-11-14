7a200b7e3553531823c0d67f5f870d16bdbe0f05
Ben Collins-Sussman <sussman@red-bean.com>
2013-11-13 23:01:18 -0600

    Actually read from microphone port and process in real time.

--- cw-decode.go
+++ cw-decode.go
@@ -4,21 +4,27 @@
   See the ALGORITHM file for a description of what's going on, and
   'proof.hs' as the original proof-of-concept implementation of this
   algorithm in Haskell.
+
+ Requirements:
+   1. Build/install portaudio C library, from http://www.portaudio.com/
+   2. go get code.google.com/p/portaudio-go/portaudio
+
+ (Originally built with 'go version go1.2rc3 darwin/amd64')
+
 */
 
 package main
 
 import (
-	// "code.google.com/p/portaudio-go/portaudio"
+	"code.google.com/p/portaudio-go/portaudio"
 	"fmt"
 	"math"
-	"math/rand"
 	"os"
 	"os/signal"
 	"sort"
 )
 
-type token int
+type token int32
 
 const (
 	dit       = iota
@@ -34,22 +40,22 @@ const (
 
 // Use Root Mean Square (RMS) method to return 'average' value of an
 // array of audio samples.
-func rms(audiovals []int) int {
-	sum := 0
-	squaresum := 0
+func rms(audiovals []int32) int32 {
+	var sum int32 = 0
+	var squaresum int32 = 0
 	for i := 0; i < len(audiovals); i++ {
 		v := audiovals[i]
 		sum = sum + v
 		squaresum = squaresum + (v * v)
 	}
-	mean := sum / len(audiovals)
-	meanOfSquares := squaresum / len(audiovals)
-	return int(math.Sqrt(float64(meanOfSquares - (mean * mean))))
+	var mean int32 = sum / int32(len(audiovals))
+	meanOfSquares := squaresum / int32(len(audiovals))
+	return int32(math.Sqrt(float64(meanOfSquares - (mean * mean))))
 }
 
 // Read audiosample chunks from 'chunks' channel, and push simple RMS
 // amplitudes into the 'amplitudes' channel.
-func amplituder(chunks chan []int, amplitudes chan int) {
+func amplituder(chunks chan []int32, amplitudes chan int32) {
 	for chunk := range chunks {
 		amplitudes <- rms(chunk)
 	}
@@ -58,11 +64,11 @@ func amplituder(chunks chan []int, amplitudes chan int) {
 
 // Read amplitudes from 'amplitudes' channel, and push quantized
 // on/off values to 'quants' channel.
-func quantizer(amplitudes chan int, quants chan bool) {
-	var group [100]int
-	seen := 0
-	max := 0
-	min := 0
+func quantizer(amplitudes chan int32, quants chan bool) {
+	var group [100]int32
+	var seen int32 = 0
+	var max int32 = 0
+	var min int32 = 0
 	for amp := range amplitudes {
 		// Suck 100 amplitudes at a time from input channel,
 		// figure out 'middle' amplitude for the group, and
@@ -91,8 +97,8 @@ func quantizer(amplitudes chan int, quants chan bool) {
 // Main stage 1 pipeline: reads audiochunks from input channel;
 // returns a boolean channel to which it pushes quantized on/off
 // values.
-func getQuantizePipe(audiochunks chan []int) chan bool {
-	amplitudes := make(chan int)
+func getQuantizePipe(audiochunks chan []int32) chan bool {
+	amplitudes := make(chan int32)
 	quants := make(chan bool)
 	go amplituder(audiochunks, amplitudes)
 	go quantizer(amplitudes, quants)
@@ -105,11 +111,11 @@ func getQuantizePipe(audiochunks chan []int) chan bool {
 // the list [3, 2, 2, 4, 2], which can be seen as the "rhythm" of the
 // coded message.
 
-func getRlePipe(quants chan bool) chan int {
-	lengths := make(chan int)
+func getRlePipe(quants chan bool) chan int32 {
+	lengths := make(chan int32)
 	go func() {
 		currentState := false
-		tally := 0
+		var tally int32 = 0
 
 		// TODO(sussman): need to "debounce" this stream
 		for quant := range quants {
@@ -129,6 +135,11 @@ func getRlePipe(quants chan bool) chan int {
 // ------- Stage 3: Figure out length of morse 'unit' & output logic tokens
 //
 
+type byInt32 []int32
+func (b byInt32) Len() int { return len(b) }
+func (b byInt32) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
+func (b byInt32) Less(i, j int) bool { return b[i] < b[j] }
+
 // Take a list of on/off duration events, sort them, return the 25th
 // percentile value as the "1 unit" duration within the time window.
 //
@@ -138,10 +149,9 @@ func getRlePipe(quants chan bool) chan int {
 // of durations. In theory we could simply pick the smallest, but by
 // going with the 25th percentile, the hope is to avoid picking the
 // ridiculously small sample that results from a quantization error.
-func calculateUnitDuration(group []int) int {
-	sort.Ints(group)
-	// fmt.Printf("(%d) ", group)
-	return group[(len(group) / 4)]
+func calculateUnitDuration(group []int32) int32 {
+	sort.Sort(byInt32(group))
+	return group[int32((len(group) / 4))]
 }
 
 // Take a normalized duration value, 'clamp' it to the magic numbers
@@ -174,7 +184,7 @@ func clamp(x float32, silence bool) token {
 	return cwError
 }
 
-func getTokenPipe(durations chan int) chan token {
+func getTokenPipe(durations chan int32) chan token {
 	tokens := make(chan token)
 	seen := 0
 	go func() {
@@ -183,7 +193,7 @@ func getTokenPipe(durations chan int) chan token {
 		//
 		// TODO(sussman): make this windowsize a constant we
 		// can fiddle.
-		group := make([]int, 20)
+		group := make([]int32, 20)
 		for duration := range durations {
 			group[seen] = duration
 			seen += 1
@@ -221,68 +231,57 @@ func main() {
 	signal.Notify(sig, os.Interrupt, os.Kill)
 
 	// main input pipe:
-	chunks := make(chan []int)
+	chunks := make(chan []int32)
 
 	// construct main output pipe... whee!
 	output := getTokenPipe(getRlePipe(getQuantizePipe(chunks)))
 
-	/*	portaudio.Initialize()
-		defer portaudio.Terminate()
-
-		in := make([]int32, 64)
-		stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(in), in)
-		chk(err)
-		defer stream.Close()
-		nSamples := 0
-
-		go func() {
-			chk(stream.Start())
-			for {
-				chk(stream.Read())
-
-				// chk(binary.Write(f, binary.BigEndian, in))
-
-				nSamples += len(in)
-				select {
-				case <-sig:
-					return
-				default:
-				}
-			}
-			chk(stream.Stop())
-		}
-	*/
-
-	// Start pushing random data into the pipeline in the background
+	// read samples from microphone, via portaudio library
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+	samplechunk := make([]int32, 64)
+	stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(samplechunk), samplechunk)
+	chk(err)
+	defer stream.Close()
+	nSamples := 0
+	
 	go func() {
-		for i := 0; i < 5000; i++ {
-			chunk := make([]int, 10)
-			for j := 0; j < 10; j++ {
-				chunk[j] = rand.Int()
+		chk(stream.Start())
+		for {
+			chk(stream.Read())
+			
+			// chk(binary.Write(f, binary.BigEndian, in))
+			chunks <- samplechunk
+		
+			nSamples += len(samplechunk)
+			select {
+			case <-sig:
+				return
+			default:
 			}
-			chunks <- chunk
 		}
-		close(chunks)
+		chk(stream.Stop())
 	}()
+	
 
 	// Print logical tokens from the pipeline's output
 	for val := range output {
 		out := ""
 		switch val {
 		case dit:
-			out = ". "
+			out = "."
 		case dah:
-			out = "_ "
+			out = "_"
 		case endLetter:
-			out = "  "
+			out = " "
 		case endWord:
-			out = ": "
+			out = " : "
 		case pause:
-			out = "pause "
+			out = " pause "
 		case noOp:
 			out = ""
 		default:
-			out = "ERROR "
+			out = " ERROR "
 		}
 		fmt.Printf("%s", out)
 	}
